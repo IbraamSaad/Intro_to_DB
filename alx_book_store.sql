448a8ec6cce8c7129bbc6f294727b55583871118
IbraamSaad <ebramb37@gmail.com>
2025-02-01 03:57:15 +0200

    alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,9 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
 
 CREATE TABLE Books (
-    book_id INT PRIMARY KEY, 
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
     title VARCHAR(130), 
     author_id INT,
     price DOUBLE,
@@ -10,26 +12,26 @@ CREATE TABLE Books (
 );
 
 CREATE TABLE Authors(
-    author_id INT PRIMARY KEY,
+    author_id INT PRIMARY KEYAUTO_INCREMENT,
     author_name VARCHAR(215),
 );
 
 CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
+    customer_id INT PRIMARY KEYAUTO_INCREMENT,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     adrress TEXT
 );
 
 CREATE TABLE Orders(
-    order_id INT PRIMARY KEY,
+    order_id INT PRIMARY KEYAUTO_INCREMENT,,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 );
 
 CREATE TABLE Order_Details(
-    order_detailed INT PRIMARY KEY,
+    order_detailed INT PRIMARY KEYAUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
