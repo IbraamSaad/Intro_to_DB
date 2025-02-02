039c56aa59e2a38c3946671c097044141fb60782
IbraamSaad <ebramb37@gmail.com>
2025-02-02 22:09:39 +0200

    populating my database

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,10 @@
 USE alx_book_store;
 
-INSERT INTO Customers(customer_id, customer_name, email, address)
-VALUES (1, Cole Baidoo, cbaidoo@sandtech.com, 123 Happiness Ave);
+INSERT INTO Customers(
+    customer_id, customer_name, email, address
+    )
+VALUES (
+    1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'
+    );
+
+SELECT * FROM customer WHERE customer_id = 1; 
