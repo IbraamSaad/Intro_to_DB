e9b9296c1e6db81d982f9f90412f076d0984b2ae
IbraamSaad <ebramb37@gmail.com>
2025-02-02 20:59:29 +0200

    print description of Books table

--- task_4.sql
+++ task_4.sql
@@ -1,11 +1,10 @@
 USE alx_book_store;
 
 SELECT 
-    book_id INT PRIMARY KEY AUTO_INCREMENT, -- Added AUTO_INCREMENT
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    book_id, title, author_id, price, publication_date
+    INT, VARCHAR, INT, DOUBLE, DATE
+    NOT NULL, NULL, NULL, NULL, NULL
+    PRIMARY KEY
+    FOREIGN KEY
 FROM
-    books;
+    Books;
